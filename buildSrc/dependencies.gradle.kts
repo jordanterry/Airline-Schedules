de74ae109a728ad279e19acc2b7541b0b6d72371
Jordan Terry <jordnullable@jordans-air.lan>
2022-04-24 09:27:01 +0100

    Use toml as version vatalog

--- buildSrc/dependencies.gradle.kts
+++ buildSrc/dependencies.gradle.kts
@@ -1,31 +1,7 @@
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("kotlin", "1.6.20")
-            version("coroutines", "1.6.0")
-            version("okhttp", "4.3.1")
-            version("retrofit", "2.7.1")
-            version("moshi", "1.9.2")
-            version("dagger", "2.40.5")
-            version("javaxinject", "1")
-            version("okio", "3.0.0")
-
-            alias("okhttp").to("com.squareup.okhttp3", "okhttp").versionRef("okhttp")
-            alias("retrofit-core").to("com.squareup.retrofit2", "retrofit").versionRef("retrofit")
-            alias("retrofit-converter-moshi").to("com.squareup.retrofit2", "converter-moshi").versionRef("retrofit")
-            alias("moshi").to("com.squareup.moshi", "moshi-kotlin").versionRef("moshi")
-
-            alias("dagger-core").to("com.google.dagger", "dagger").versionRef("dagger")
-            alias("dagger-compiler").to("com.google.dagger", "dagger-compiler").versionRef("dagger")
-            alias("inject").to("javax.inject", "javax.inject").versionRef("javaxinject")
-
-            alias("coroutines").to("org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef("coroutines")
-
-            alias("okio").to("com.squareup.okio", "okio").versionRef("okio")
-
-            bundle("async", listOf("coroutines"))
-            bundle("networking", listOf("okhttp", "retrofit-core", "retrofit-converter-moshi", "moshi"))
-            bundle("io", listOf("okio"))
+            from(files("../deps/libs.versions.toml"))
         }
     }
 }
